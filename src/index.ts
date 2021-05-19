c24ecab4b0205ba3cd69476ac03dc81faf847578
mabrur-h <mabrur.h11@gmail.com>
2021-05-19 16:41:58 +0500

    🚀 setup express server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-console.log('hi');
+import * as dotenv  from 'dotenv';
+import express from 'express';
 
-if("llllll") {
-    console.log('as')
-}
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+
+// define route handler for the default home page
+app.use('/', (req, res) => {
+  res.send('working')
+})
+
+// start the express server
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+})
